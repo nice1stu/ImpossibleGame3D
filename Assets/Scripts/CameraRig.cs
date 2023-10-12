9031d73c13df90a3d0bb6ca3687b925e3cc3aaac
nice1stu <112468923+nice1stu@users.noreply.github.com>
2023-10-13 01:35:43 +0200

    Update CameraRig.cs

--- Assets/Scripts/CameraRig.cs
+++ Assets/Scripts/CameraRig.cs
@@ -21,7 +21,7 @@ public class CameraRig : MonoBehaviour, IPlayerTracking
         if (_isPlayerTransformNotNull)
         {
             var position = playerTransform.position;
-            Vector3 newPosition = new Vector3(position.x, position.y + 6.5f, (position.z-4));
+            Vector3 newPosition = new Vector3(position.x, position.y + 6.5f, (position.z-5));
             var rotation = transform.rotation;
             rotation = Quaternion.Euler(+35, rotation.eulerAngles.y, rotation.eulerAngles.z);
             var transform1 = transform;
