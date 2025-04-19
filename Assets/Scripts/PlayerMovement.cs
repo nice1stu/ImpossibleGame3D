c03a4469a44225abee341c1c32851ad2e8f3c6be
Stewart Wan <nice1stu@gmail.com>
2025-04-19 18:27:19 +0200

    Update PlayerMovement.cs
    Fixed Player rotation on jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public bool isGrounded = true;
-    
+
     private Rigidbody _rb;
-    
+    private bool _isJumping;
+    private Quaternion _targetRotation;
+    private Quaternion _startRotation;
+    private const float RotationDuration = 1.0f;
+    private float _rotationTimer;
+    private bool _rotating;
+
     private readonly float _jumpForce = 7f;
     private readonly float _fallingForce = -15f;
-    private readonly float _rotationSpeed = 14f;
 
     private void Start()
     {
@@ -18,24 +23,38 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         if (_rb.linearVelocity.y < -0.1f)
-            _rb.AddForce(0,_fallingForce,0);
+            _rb.AddForce(0, _fallingForce, 0);
+
+        if (_rotating)
+        {
+            _rotationTimer += Time.fixedDeltaTime;
+            float t = Mathf.Clamp01(_rotationTimer / RotationDuration);
+            transform.rotation = Quaternion.Slerp(_startRotation, _targetRotation, t);
+
+            if (t >= 1f)
+            {
+                _rotating = false;
+            }
+        }
     }
 
     private void Update()
     {
         isGrounded = Physics.Raycast(transform.position, Vector3.down, out _, 1.5f);
 
-        switch (isGrounded)
+        if (isGrounded && Input.GetButtonDown("Jump") && !_isJumping)
         {
-            case true when Input.GetButtonDown("Jump"):
-                _rb.linearVelocity = new Vector3(0, _jumpForce, 0);
-                break;
-            case false:
-            {
-                float desiredAngularVelocity = _rotationSpeed * Mathf.PI / 180f;
-                _rb.AddTorque(transform.right * desiredAngularVelocity);
-                break;
-            }
+            _rb.linearVelocity = new Vector3(_rb.linearVelocity.x, _jumpForce, _rb.linearVelocity.z);
+            _isJumping = true;
+            _rotating = true;
+            _startRotation = transform.rotation;
+            _targetRotation = _startRotation * Quaternion.Euler(-180f, 0f, 0f); // Rotate 180 degrees around local x-axis (forward)
+            _rotationTimer = 0f;
+        }
+
+        if (_isJumping && isGrounded)
+        {
+            _isJumping = false;
         }
     }
 }
